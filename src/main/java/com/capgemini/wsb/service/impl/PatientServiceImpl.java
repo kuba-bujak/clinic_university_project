968d19418e5d8de05702d43425a0a1df66f50496
Jakub Bujak <kuba.bujak0@wp.pl>
2024-05-04 14:15:23 +0200

    Implemented PatientService

---
+++ src/main/java/com/capgemini/wsb/service/impl/PatientServiceImpl.java
@@ -0,0 +1,29 @@
+package com.capgemini.wsb.service.impl;
+
+import com.capgemini.wsb.dto.PatientTO;
+import com.capgemini.wsb.mapper.PatientMapper;
+import com.capgemini.wsb.persistence.dao.PatientDao;
+import com.capgemini.wsb.persistence.entity.PatientEntity;
+import com.capgemini.wsb.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class PatientServiceImpl implements PatientService {
+
+    private final PatientDao patientDao;
+
+    @Autowired
+    public PatientServiceImpl(PatientDao patientRepository) {
+        this.patientDao = patientRepository;
+    }
+
+    @Override
+    public PatientTO findById(Long id) {
+        final PatientEntity patientEntity = patientDao.findOne(id);
+        return PatientMapper.mapToTO(patientEntity);
+    }
+}
