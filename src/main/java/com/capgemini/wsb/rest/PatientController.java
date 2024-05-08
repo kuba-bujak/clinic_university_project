6e3590c94deeb086ae03bffcc0c997d75ee5a562
Jakub Bujak <jakub.bujak@volvo.com>
2024-05-09 00:01:23 +0200

    Implemented REST

--- src/main/java/com/capgemini/wsb/rest/PatientController.java
+++ src/main/java/com/capgemini/wsb/rest/PatientController.java
@@ -1,9 +1,49 @@
 package com.capgemini.wsb.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.capgemini.wsb.dto.PatientTO;
+import com.capgemini.wsb.rest.exception.EntityNotFoundException;
+import com.capgemini.wsb.service.PatientService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/patients")
 public class PatientController {
+
+    private final PatientService patientService;
+
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping("/patients")
+    List<PatientTO> findAllPatients() {
+        final List<PatientTO> listOfPatients = patientService.getPatientList();
+        return listOfPatients;
+    }
+
+    @PostMapping("/patient/{id}")
+    PatientTO updatePatient(@PathVariable final Long id, PatientTO updatedPatientTO) throws Exception {
+        final PatientTO patientTO = patientService.findById(id);
+        if (patientTO != null) {
+            PatientTO updatedPatient = patientService.updatePatient(updatedPatientTO);
+            if (updatedPatient != null) {
+                return updatedPatient;
+            }
+            throw new Exception("Patient was not updated");
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    @GetMapping("/patient/{id}")
+    PatientTO findPatientById(@PathVariable final Long id){
+        final PatientTO patientTO = patientService.findById(id);
+        if (patientTO != null) {
+            return patientTO;
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+
 }
